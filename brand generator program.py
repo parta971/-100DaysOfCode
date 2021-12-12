13e04d44d22998848a63ce6e3a3a4061724695d4
Prachi Tiwari <34484340+parta971@users.noreply.github.com>
2021-12-12 22:19:41 +0530

    Add files via upload

---
+++ brand generator program.py
@@ -0,0 +1,6 @@
+#Band Generator
+print("Hello!\nPlease enter your name:")
+a=input()
+print("Please enter the place where you were born:")
+b=input()
+print('The name of your band can be-\n'+a+' '+b)

ed26eeafdf1486cd11afa1be37c9539c6c5a0803
Prachi Tiwari <34484340+parta971@users.noreply.github.com>
2021-12-16 00:23:22 +0530

    Add files via upload

---
+++ Exercise 3 - Life in Weeks.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+age = int(input("What is your current age?"))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+years_left=90-age
+
+months=12*years_left
+weeks=52*years_left
+days=365*years_left
+
+print (f"You have {days} days, {weeks} weeks, and {months} months left")
+

034747ad8153e0753add1370544255d2a12a027e
Prachi Tiwari <34484340+parta971@users.noreply.github.com>
2022-01-07 02:39:19 +0530

    Add files via upload

---
+++ Day 14 Project Higher-Lower-Game.py
@@ -0,0 +1,49 @@
+from game_data import data
+import random
+from art import logo, vs
+from replit import clear
+
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
+
+def check_answer(guess, a_followers, b_followers):
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+
+
+def game():
+  print(logo)
+  score = 0
+  game_should_continue = True
+  account_a = get_random_account()
+  account_b = get_random_account()
+
+  while game_should_continue:
+    account_a = account_b
+    account_b = get_random_account()
+
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"Compare A: ",account_a['name']," a ",account_a['description']," from ",account_a['country'])
+    print(vs)
+    print(f"Against B: ",account_b['name']," a ",account_b['description']," from ",account_b['country'])
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    clear()
+    print(logo)
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
+
+game()
